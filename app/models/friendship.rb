f0bcf18a5087152abe77c30f649b3f342088f495
Elliot Yardley <eyardley27@gmail.com>
2026-02-28 11:53:01 -0800

    Added validation to prevent reverse friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,14 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  validate :no_reverse_friendship, on: :create
+
+
+  private
+
+  def no_reverse_friendship
+    if Friendship.where(user: friend, friend: user).exists?
+      errors.add(:base, "A friendship with this user already exists")
+  end
 end
